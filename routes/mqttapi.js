c2d26d0029e3d2820e3203c8cdc4f13dfcff8264
ahmadherdiansyah <ipgateway404@gmail.com>
2018-09-13 13:46:42 +0700

    change server to XL IoT Platform

--- routes/mqttapi.js
+++ routes/mqttapi.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mqtt = require('mqtt');
-var client = mqtt.connect({ port: 1883, host: '192.168.46.3', keepalive: 10000})
+var client = mqtt.connect({ port: 1883, host: 'mqtt.flexiot.xl.co.id', keepalive: 10000})
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -27,6 +27,54 @@ router.post('/publish',function(req,res,next) {
       "pesan": "Semua atribut harus di isi"
     }]);
   }
-})
-
+});
+router.get('/publish',function(req,res,next) {
+  var top = req.query.topic ;
+  var event = req.query.event;
+  var pesan = req.query.pesan;
+  var mac = req.query.mac;
+  if (top && event && pesan && mac ) {
+    var all = "{\"eventName\":\""+event+"\","+pesan+",\"mac\":\""+mac+"\"}";
+    client.publish(top, all,function (err){
+      if (err) {
+        res.send(err);
+      }
+      else{
+        res.json([{
+          "status": "sukses",
+          "topic": req.body.topic,
+          "pesan": req.body.pesan
+        }]);
+      }
+    }); 
+  }else{
+    res.status(400);
+    res.json([{
+      "status": "Errors 400",
+      "pesan": "Semua atribut harus di isi"
+    }]);
+  }
+});
+router.post('/subscibe',function(req,res,next) {
+  if (req.body.topic && req.body.authtoken) {
+    client.publish(req.body.topic, req.body.pesan,function (err){
+      if (err) {
+        res.send(err);
+      }
+      else{
+        res.json([{
+          "status": "sukses",
+          "topic": req.body.topic,
+          "pesan": req.body.pesan
+        }]);
+      }
+    }); 
+  }else{
+    res.status(400);
+    res.json([{
+      "status": "Errors 400",
+      "pesan": "Semua atribut harus di isi"
+    }]);
+  }
+});
 module.exports = router;
