f7a522f76f53a3eebec8bc49469b47bcd5adc46d
ahmadherdiansyah <ipgateway404@gmail.com>
2018-09-13 10:52:31 +0700

    fix bug

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules/bootstrap')));
 app.use(express.static(path.join(__dirname, 'node_modules/jquery')));
-app.use('/', indexRouter);
+app.use('/', login);
 app.use('/users', usersRouter);
 app.use('/api',apiRoouter);
 app.use('/api/mqtt',mqttapi);
